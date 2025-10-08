bd28fe604e1fd9fe83d6713955b45922cc9af371
minnulekha <minnulekhavg@gmail.com>
2025-10-08 21:24:44 +0530

    Setupped Cloudinary

--- nursery_project/settings.py
+++ nursery_project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,8 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-from pathlib import Path
+
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
